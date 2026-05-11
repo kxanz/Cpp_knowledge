3bcea0b5305a6dfd7061cf6ef4b6c68bd24e076f
danipicks <danipicks04@gmail.com>
2026-05-10 21:08:10 -0600

    Undefined Behavior

---
+++ Undefined Behavior/ub.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<iostream>
+
+int main(){
+int i;
+char c;
+double d;
+float f;
+bool b;
+std::cout << "int	:" << i << "\nchar	:" << c << "\ndouble	:" << d << "\nfloat	:" << f << "\nbool	:" << b;
+return 0;
+};
