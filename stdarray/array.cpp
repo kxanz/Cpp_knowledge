f6268f8558aa64392096fa2c4c1f2db3f52d5c6b
danipicks <danipicks04@gmail.com>
2026-07-10 16:29:56 -0600

    array

---
+++ stdarray/array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int> vec;
+    vec.push_back(4);
+    vec.push_back(1);
+    vec.push_back(8);
+
+    vector<int>::iterator itr1 = vec.begin();
+    vector<int>::iterator itr2 = vec.end();
+
+    for (vector<int>::iterator itr = itr1; itr!=itr2; ++itr)
+        cout << *itr << " "; 
+
+    sort(itr1, itr2); 
+
+
+    return 0;
+}
